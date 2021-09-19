c2081fd8d75a9d28851f19187242734e3f9b0ddf
Suleman785 <sulemanserai890@gmail.com>
2021-09-19 00:41:03 -0700

    maeksheet

---
+++ Marks.java
@@ -0,0 +1,20 @@
+public  class Marks {
+    public static void main(String[] args) {
+
+        int percentage=55;
+
+        if (percentage>80){
+            System.out.println("Great work you got A plus");
+        }
+        else if (percentage>=70) {
+            System.out.println("sahi aw you got B grade");
+        }
+        else if (percentage>=50) {
+            System.out.println("mazo nahe yawr you got c grade");
+        }
+        else if (percentage>=50){
+            System.out.println("lagay tho hamrah khy $ kamer jo shonl aw");
+        }
+
+    }
+}
